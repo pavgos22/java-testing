c23ecad8e15f5c790ae9fe55c3dce14bc0c5f22a
Paweł Gościak <pawgosciak@gmail.com>
2024-02-07 14:10:35 +0100

    8.2.0

---
+++ java-exception/src/main/java/com/exception/main/ExceptionModuleRunner.java
@@ -0,0 +1,11 @@
+package com.exception.main;
+
+import com.exception.io.FileReader;
+
+public class ExceptionModuleRunner {
+
+    public static void main(String[] args) {
+        FileReader fileReader = new FileReader();
+        fileReader.readFile();
+    }
+}
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -2,3 +2,6 @@ rootProject.name = "java-testing"
 include("junit-testing")
 include("java-stream")
 include("java-rts")
+include("java-rts:java-exception")
+findProject(":java-rts:java-exception")?.name = "java-exception"
+include("java-exception")
