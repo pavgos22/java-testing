a7d28e3c871aaec0bc0fe6294d4009fd40d65324
Paweł Gościak <pawgosciak@gmail.com>
2024-02-08 23:47:14 +0100

    8.3.0

--- java-exception/src/main/java/com/exception/main/ExceptionModuleRunner.java
+++ java-exception/src/main/java/com/exception/main/ExceptionModuleRunner.java
@@ -1,11 +1,12 @@
 package com.exception.main;
 
 import com.exception.io.FileReader;
+import com.exception.io.FileReaderWithoutHandling;
 
 public class ExceptionModuleRunner {
 
     public static void main(String[] args) {
-        FileReader fileReader = new FileReader();
+        FileReaderWithoutHandling fileReader = new FileReaderWithoutHandling();
         fileReader.readFile();
     }
 }
