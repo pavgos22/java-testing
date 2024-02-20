b045b698f0ff6074a2cbdd463a2aa18a4276c6e5
Paweł Gościak <pawgosciak@gmail.com>
2024-02-20 11:15:34 +0100

    14.1.1

--- java-patterns/src/test/java/com/patterns/singleton/LoggerTestSuite.java
+++ java-patterns/src/test/java/com/patterns/singleton/LoggerTestSuite.java
@@ -6,11 +6,15 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 public class LoggerTestSuite {
 
     @Test
-    public void testGetLastLog() {
-        Logger logger = Logger.getInstance();
+    public void testSingletonLoggerLastLog() {
+        Logger logger1 = Logger.getInstance();
+        Logger logger2 = Logger.getInstance();
+        Logger logger3 = Logger.getInstance();
 
-        logger.log("Test log");
+        logger1.log("Log from logger1");
+        logger2.log("Log from logger2");
+        logger3.log("Log from logger3");
 
-        assertEquals("Test log", logger.getLastLog());
+        assertEquals("Log from logger3", logger1.getLastLog());
     }
 }
