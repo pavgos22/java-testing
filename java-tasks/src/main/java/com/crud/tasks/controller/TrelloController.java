dff8efb8e4b4b6e53bf7623022646348a03dbad4
Paweł Gościak <pawgosciak@gmail.com>
2024-04-25 21:53:25 +0200

    21.2.0

---
+++ java-tasks/src/main/java/com/crud/tasks/controller/TrelloController.java
@@ -0,0 +1,30 @@
+package com.crud.tasks.controller;
+
+import com.crud.tasks.domain.TrelloBoardDto;
+import com.crud.client.TrelloClient;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@RestController
+@RequestMapping("v1/trello")
+@RequiredArgsConstructor
+@CrossOrigin("*")
+public class TrelloController {
+
+    private final TrelloClient trelloClient;
+
+    @GetMapping("boards")
+    public void getTrelloBoards() {
+        List<TrelloBoardDto> trelloBoards = trelloClient.getTrelloBoards();
+        trelloBoards.stream()
+                .filter(board -> board.getName() != null && board.getId() != null && board.getName().contains("Kodilla"))
+                .forEach(board -> System.out.println(board.getId() + " " + board.getName()));
+    }
+}
+
