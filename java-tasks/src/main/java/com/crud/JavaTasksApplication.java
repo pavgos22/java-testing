b18ec33bba8d5eac08426315d0c067b52f52e686
Paweł Gościak <pawgosciak@gmail.com>
2024-04-30 00:26:11 +0200

    22.4.0

--- java-tasks/src/main/java/com/crud/JavaTasksApplication.java
+++ java-tasks/src/main/java/com/crud/JavaTasksApplication.java
@@ -3,9 +3,11 @@ package com.crud;
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @OpenAPIDefinition
 @SpringBootApplication
+@EnableScheduling
 public class JavaTasksApplication {
 
     public static void main(String[] args) {
